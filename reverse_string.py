0eadf3ee42b8e28db0d7389e5696693148c421d5
souvik-wizard <dassouvik3327@gmail.com>
2022-07-02 20:57:56 +0530

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,6 @@
+
+def reverseString(self, s: List[str]) -> None:
+        length = len(s)
+        for item in range(length//2):
+            s[item], s[length-1-item] = s[length-1-item], s[item]
+           
