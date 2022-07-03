0eb91b53932d5146581c16a99b9d9ab17824eb5b
souvik-wizard <dassouvik3327@gmail.com>
2022-07-03 20:13:45 +0530

    TC reduced

--- Minimum Number of Moves to Seat Everyone.py
+++ Minimum Number of Moves to Seat Everyone.py
@@ -5,11 +5,13 @@ def minMovesToSeat(seats, students):
         seats.sort()
         students.sort()
         if len(seats) == len(students):
-            moves = 0
-        for i, j in zip(seats, students):
-            if i!= j:
-                moves += abs(i-j)
-        return moves
+            moves = 0                     
+            for i, j in zip(seats, students):
+                if i!= j:
+                    moves += abs(i-j)
+            return moves
+
+            
         
 seats=[4,1,5,9]
 students=[1,3,2,6]
