6f54b1e26c0bef7412783559a334049361e0510e
souvik-wizard <dassouvik3327@gmail.com>
2022-07-03 16:17:02 +0530

    Create Minimum Number of Moves to Seat Everyone.py

---
+++ Minimum Number of Moves to Seat Everyone.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+
+
+def minMovesToSeat(seats, students):
+        seats.sort()
+        students.sort()
+        if len(seats) == len(students):
+            moves = 0
+        for i, j in zip(seats, students):
+            if i!= j:
+                moves += abs(i-j)
+        return moves
+        
+seats=[4,1,5,9]
+students=[1,3,2,6]
+
+print(minMovesToSeat(seats,students))
