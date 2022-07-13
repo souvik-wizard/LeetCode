fb1209a5ffe529d422a62198a4ce5532f3e04335
souvik-wizard <dassouvik3327@gmail.com>
2022-07-13 23:19:00 +0530

    Another solution added

--- 34-First and Last Position of Element in Sorted Array .py
+++ 34-First and Last Position of Element in Sorted Array .py
@@ -1,11 +1,31 @@
-#Solution 1
+#Solution 1 [using binary search]
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left=self.binarySearch(nums,target,True)
+        right=self.binarySearch(nums,target,False)
+        return [left,right]
+        
+    def binarySearch(self,nums,target,leftBias): 
+        low,high=0, len(nums)-1
+        res=-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>target:
+                high=mid-1
+            elif nums[mid]<target:
+                low=mid+1
+            else:
+                res=mid
+                if leftBias:
+                    high=mid-1
+                else:
+                    low=mid+1
+        return res
+                    
 
 
+#Solution 2 [Using bisect method] --I personally liked it
 
-
-
-
-#Solution 2
 
 import bisect
 
