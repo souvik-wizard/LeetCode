1f0176439929a488254149d4b4b9ca2e9a3246df
souvik-wizard <dassouvik3327@gmail.com>
2022-07-14 14:09:50 +0530

    Create 367-Valid Perfect Square.py

---
+++ 367-Valid Perfect Square.py
@@ -0,0 +1,12 @@
+def isPerfectSquare(self, num: int) -> bool:
+    low=0
+    high=num
+    while low<=high:
+        mid=(low+high)//2
+        if mid*mid==num:
+            return True
+        elif mid*mid>num:
+            high=mid-1
+        else:
+            low=mid+1
+    return False
