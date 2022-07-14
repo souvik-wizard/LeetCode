c22aa8b16b20358550799cb9a980ca3581462156
souvik-wizard <dassouvik3327@gmail.com>
2022-07-14 12:57:05 +0530

    Create 69-Sqrt(x).py

---
+++ 69-Sqrt(x).py
@@ -0,0 +1,19 @@
+# First  method  - I think I invented, NVM :)
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return math.floor(x**0.5)
+
+
+
+
+
+# Alternative method
+
+
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return math.floor(math.sqrt(x))
+
