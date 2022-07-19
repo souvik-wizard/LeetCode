1dce4ceb4603b866bbc00b247f1c8e45425abf6e
souvik-wizard <dassouvik3327@gmail.com>
2022-07-19 20:57:44 +0530

    Create 4-Median of Two Sorted Arrays.py

---
+++ 4-Median of Two Sorted Arrays.py
@@ -0,0 +1,15 @@
+import math
+def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    nums1=nums1+nums2
+    nums1.sort()
+    length=len(nums1)       
+    if (length%2)==0:
+        low=0
+        high=length-1
+        while low<=high:
+            low+=1
+            high-=1
+        return (nums1[low]+nums1[high])/2
+    else:
+        mid=math.ceil(length//2)
+        return nums1[mid]
