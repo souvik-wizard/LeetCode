ffd96debe30a897460e2accc4ce6259343d92bee
souvik-wizard <dassouvik3327@gmail.com>
2022-07-21 23:32:16 +0530

    Create 509-Fibonacci Number.py

---
+++ 509-Fibonacci Number.py
@@ -0,0 +1,15 @@
+# Using while loop
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0 or n==1:
+            return n
+        start=0
+        end=1
+        s=start+end
+        while (n>1):
+            s=start+end
+            start=end
+            end=s
+            n-=1
+        return s
