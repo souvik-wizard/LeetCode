48eb25e4740585017d9466c006db2d474cc5d710
souvik-wizard <dassouvik3327@gmail.com>
2022-07-31 00:06:39 +0530

    added some comments

--- 66-Plus One.py
+++ 66-Plus One.py
@@ -1,4 +1,8 @@
 class Solution:
-    def plusOne(self, arr: List[int]) -> List[int]:
-            res = int("".join(map(str, arr)))           
+    def plusOne(self, arr: List[int]) -> List[int]: 
+
+            # Converting integer list to string list and joining the list using join()
+            res = int("".join(map(str, arr)))   
+
+            #list comprehension 
             return [int(x) for x in str(int(res+1))] 
