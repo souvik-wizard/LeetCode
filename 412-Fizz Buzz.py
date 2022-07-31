df9d868b9055713e7897078515bd883c43032184
souvik-wizard <dassouvik3327@gmail.com>
2022-07-31 12:47:32 +0530

    Create 412-Fizz Buzz.py

---
+++ 412-Fizz Buzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+            arr=[]        
+            for i in range(1,n+1):
+                if i%3==0 and i%5==0:
+                    arr.append("FizzBuzz")
+                elif i%3==0:
+                    arr.append("Fizz")
+                elif i%5==0:
+                    arr.append("Buzz")
+                else:
+                    arr.append(str(i))
+            return arr
+
+            # return ([str(elem) for elem in arr])   --- In case if u didn't append the numbers as string 
