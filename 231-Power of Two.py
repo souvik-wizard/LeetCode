25ce878b898281b0d7ccd2c60e78e308ea0e8472
souvik-wizard <dassouvik3327@gmail.com>
2022-08-01 20:29:31 +0530

    Create 231-Power of Two.py

---
+++ 231-Power of Two.py
@@ -0,0 +1,13 @@
+import math
+
+def isPowerOfTwo(n):
+            if n<=0: return False
+            num=math.log2(n)
+            if num == int(num):return True
+            else: return False
+            
+
+if __name__=='__main__':
+    n=-2
+
+    print(isPowerOfTwo(n))
