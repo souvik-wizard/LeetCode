80bbb623fabc717aa8f9234a23b13100a488c87b
souvik-wizard <dassouvik3327@gmail.com>
2022-08-01 19:38:02 +0530

    Update 66-Plus One.py

--- 66-Plus One.py
+++ 66-Plus One.py
@@ -1,6 +1,8 @@
 class Solution:
     def plusOne(self, arr: List[int]) -> List[int]: 
 
+    
+            # Python String join() Method
             # Converting integer list to string list and joining the list using join()
             res = int("".join(map(str, arr)))   
 
