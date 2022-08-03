c8275d283e250c63d662a3cfdcc6d4c6c61636b9
souvik-wizard <dassouvik3327@gmail.com>
2022-08-03 23:09:20 +0530

    Create 746-Min Cost Climbing Stairs.py

---
+++ 746-Min Cost Climbing Stairs.py
@@ -0,0 +1,29 @@
+def minCostClimbingStairs(cost):
+    cost.append(0)
+    for i in range(len(cost)-3,-1,-1):          #range(initial , final[-1 bcz we need to go to index 0] , jump)
+                
+        cost[i]+= min(cost[i+1],cost[i+2])
+
+    return min(cost[0],cost[1])
+
+
+
+
+
+
+if __name__=="__main__":
+    cost=[1,100,1,1,1,100,1,1,100,1]
+    print(minCostClimbingStairs(cost))
+
+
+
+
+
+
+
+
+
+
+# This example may help to understand [debug it for better understanding] || take final -1 & 0 
+for x in range(10, -1, -1):
+  print(x) 
