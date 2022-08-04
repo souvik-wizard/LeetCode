2cc4b7ec21b41d4850de4fde47928da67a779c85
souvik-wizard <dassouvik3327@gmail.com>
2022-08-04 21:18:48 +0530

    Update 2032-Two Out of Three.py

--- 2032-Two Out of Three.py
+++ 2032-Two Out of Three.py
@@ -1,14 +1,35 @@
-
-def twoOutOfThree(self, num1: List[int], num2: List[int], num3: List[int]) -> List[int]:
+# my solution
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
         
         final=[]
-        for i in range (len(num1)):
-            if  num1[i] in num2:
-                final.append(num1[i])
-        for i in range (len(num2)):
-            if  num2[i] in num3:
-                final.append(num2[i])
-        for i in range (len(num3)):
-            if  num3[i] in num1:
-                final.append(num3[i])
+        for i in range (len(nums1)):
+            if  nums1[i] in nums2:
+                final.append(nums1[i])
+        for i in range (len(nums2)):
+            if  nums2[i] in nums3:
+                final.append(nums2[i])
+        for i in range (len(nums3)):
+            if  nums3[i] in nums1:
+                final.append(nums3[i])
         return set(final)
+
+
+# Some learned methods
+
+# Concept of intersaction and union from mathematics
+def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+    set1,set2,set3 = set(nums1),set(nums2),set(nums3)
+    
+    a = list(set1.intersection(set2))
+    b = list(set1.intersection(set3))
+    c = list(set2.intersection(set3))
+    
+    
+    return list(set(a+b+c))
+
+
+# Concept of bitwise operators
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        return set(nums1) & set(nums2) | set(nums2) & set(nums3) | set(nums1) & set(nums3)
