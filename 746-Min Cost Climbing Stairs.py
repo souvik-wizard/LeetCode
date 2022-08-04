984ee6fbf3867f3a895d1365be9a8ca345982408
souvik-wizard <dassouvik3327@gmail.com>
2022-08-04 19:40:16 +0530

    Update 746-Min Cost Climbing Stairs.py

--- 746-Min Cost Climbing Stairs.py
+++ 746-Min Cost Climbing Stairs.py
@@ -1,3 +1,5 @@
+# Beginer friendly
+
 def minCostClimbingStairs(cost):
     cost.append(0)
     for i in range(len(cost)-3,-1,-1):          #range(initial , final[-1 bcz we need to go to index 0] , jump)
@@ -7,6 +9,18 @@ def minCostClimbingStairs(cost):
     return min(cost[0],cost[1])
 
 
+# sol 2 [learned]
+
+def minCostClimbingStairs(cost):
+    p0=cost[0]
+    p1=cost[1]
+    
+    for i in range (2, len(cost)):
+        temp=cost[i]+ min(p1,p0)
+        p0=p1
+        p1=temp
+    return min(p0,p1)
+
 
 
 
@@ -14,12 +28,6 @@ def minCostClimbingStairs(cost):
 if __name__=="__main__":
     cost=[1,100,1,1,1,100,1,1,100,1]
     print(minCostClimbingStairs(cost))
-
-
-
-
-
-
 
 
 
