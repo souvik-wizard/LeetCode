456b2639536a670223f3a3433790c0c84ca799a9
souvik-wizard <dassouvik3327@gmail.com>
2022-08-08 20:17:09 +0530

    Update 709-To Lower Case.py

--- 709-To Lower Case.py
+++ 709-To Lower Case.py
@@ -1,3 +1,34 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
         return s.lower()
+
+
+# Another Approach learned [USING ASCII VALUE]
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+            d = ''
+            for i in s:
+                if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+                    # Why 32?
+                    # If we ADD 65+32 we get the small value for Capital A (Check ascii table for more info)
+                    d += chr(ord(i) + 32)
+                else:
+                    d += i
+            return d  
+
+
+#OR
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        ch = ""
+        for i in s:
+            # ord() use to get the ascii value in python
+            asc = ord(i)
+            # Capital letters in ascii table starts from 64-90
+            # Small letters in ascii table starts from 97-122
+            if asc > 64 and asc < 91:
+                ch += chr(asc+32)
+            else:
+                ch +=chr(asc)
+        return ch
