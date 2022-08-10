586b383be10cd99d64cd412850c763b223fefdcf
souvik-wizard <dassouvik3327@gmail.com>
2022-08-10 15:53:00 +0530

    Update 217-Contains Duplicate.py

--- 217-Contains Duplicate.py
+++ 217-Contains Duplicate.py
@@ -1,3 +1,5 @@
+#Sol 1
+# Using hash dict
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hash_dict={}
@@ -6,3 +8,9 @@ class Solution:
                 return True
             hash_dict[nums[i]]=[i]
         return False
+
+# Sol 2
+# Set concept can be use here as duplicate values can't be store in a set.
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums)!= len(set(nums))
