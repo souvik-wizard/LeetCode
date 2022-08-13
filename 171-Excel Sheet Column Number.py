8742c6fedfc1ce1416c1eb844d3caf395f4cb453
souvik-wizard <dassouvik3327@gmail.com>
2022-08-13 19:56:34 +0530

    .

--- 171-Excel Sheet Column Number.py
+++ 171-Excel Sheet Column Number.py
@@ -1,33 +1,25 @@
-class Solution:
-    def titleToNumber(self, title: str) -> int:
-        dict={
-            'A':1,
-            'B':2,
-            'C':3,
-            'D':4,
-            'E':5,
-            'F':6,
-            'G':7,
-            'H':8,
-            'I':9,
-            'J':10,
-            'K':11,
-            'L':12,
-            'M':13,
-            'N':14,
-            'O':15,
-            'P':16,
-            'Q':17,
-            'R':18,
-            'S':19,
-            'T':20,
-            'U':21,
-            'V':22,
-            'W':23,
-            'X':24,
-            'Y':25,
-            'Z':26
-        }
-        for i in title:
-            title([dict[i]]**[i])
-        print (len(title))
+def titleToNumber(self, title: str) -> int:
+        res=0        
+        for i in range(len(title)):
+            # as we all know that there are 26 alphabets in English thats why 26.
+            # why pre-multiply 26 with res?
+            #if there is given only one single alphabet ["A" -> 1] then we first multiply 26 with 0 and that will be 0 itself.
+            # And after that res will be updated  with its actual value and the loop ends. 
+            # Why multiply with 26 only?
+            #if it is lets say ("BAD") then we'll multiply B's value with 26 -->26 * 2(B's value) 
+            res*=26
+
+            # Why ASCII?  Bcz of that we dont need to use any extra space such as hash table to store A-Z value individually.
+            # we are adding the Value of that particular character [ord(title[i]))] with our res.
+            # Why substract with Ord("A")?  cz we need the exact value for our character [title[i])].
+            # And we can get that if and only if we substarct that value with "A"s value.
+            # Why Then add +1 at last?
+            # If [title[i]] is "A" itself then A will be 0 , And we know "A" is 1.
+            res+=ord(title[i])-ord("A")+1
+
+
+
+            # Instead of writing {ord("A")+1} in previous expression we can write this.That will do the same for us.
+            # res+=ord(title[i])-64
+
+        return res
