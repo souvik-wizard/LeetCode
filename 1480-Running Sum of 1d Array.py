f4a7385051e6e85cfdd3b99ff351347d5b387edd
souvik-wizard <dassouvik3327@gmail.com>
2022-08-22 22:37:20 +0530

    Create 1480-Running Sum of 1d Array.py

---
+++ 1480-Running Sum of 1d Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(n-1):
+            nums[i+1] = nums[i]+nums[i+1]
+        return nums
