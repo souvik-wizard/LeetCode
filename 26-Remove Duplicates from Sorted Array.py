0a7d5c9b7c9108a3e24a6947e1f91ca7285088b6
souvik-wizard <dassouvik3327@gmail.com>
2022-08-26 21:38:41 +0530

    Update 26-Remove Duplicates from Sorted Array.py

--- 26-Remove Duplicates from Sorted Array.py
+++ 26-Remove Duplicates from Sorted Array.py
@@ -6,6 +6,17 @@ class Solution:
                 pointer+=1
                 nums[pointer]=nums[i]
                 # print(nums[pointer])
+        # print(pointer)
         return pointer+1
         
             
+# easy to understand solution
+class Solution(object):
+    def removeDuplicates(self, nums):
+        k = 1
+        while k < len(nums): 
+            if nums[k - 1] == nums[k]:
+                nums.pop(k)
+            else:
+                k += 1
+        return k
