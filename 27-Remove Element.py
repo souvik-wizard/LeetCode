722ba04a329b1b51f707b2f8dba4d3d67412a41b
souvik-wizard <dassouvik3327@gmail.com>
2022-08-27 22:50:23 +0530

    Update 27-Remove Element.py

---
+++ 27-Remove Element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        pointer=0
+        while pointer<len(nums):
+            if nums[pointer]==val:
+                nums.pop(pointer)
+            else:   
+                pointer+=1
+        return pointer
