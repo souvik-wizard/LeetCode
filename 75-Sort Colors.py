573d2fc1c1f0e2fc777486421f97c28a633821db
souvik-wizard <dassouvik3327@gmail.com>
2022-08-30 21:19:13 +0530

    Update 75-Sort Colors.py

---
+++ 75-Sort Colors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero,one,two = 0,0,0
+        for i in range(len(nums)):
+            if(nums[i] == 0): zero +=1
+            elif(nums[i] == 1): one +=1
+            elif(nums[i] == 2): two +=1
+        s = "0"*zero + "1"*one + "2"*two
+        # print(s)
+        for i in range(len(s)):
+            nums[i] = s[i]
