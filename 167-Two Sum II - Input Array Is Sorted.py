b4575d81f1fdf8333741c6d7f9c7bf70724439c2
souvik-wizard <dassouvik3327@gmail.com>
2022-09-02 21:03:42 +0530

    Create 167-Two Sum II - Input Array Is Sorted.py

---
+++ 167-Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,13 @@
+# Easy to understand , simple binary search
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            if nums[low]+nums[high]==target:
+                return low+1,high+1
+            elif nums[low]+nums[high]<target:
+                low+=1
+            else:
+                high-=1
+                
