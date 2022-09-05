74cf87a41429f1f18f54be345dfe44779f96d8c1
souvik-wizard <dassouvik3327@gmail.com>
2022-09-05 20:21:31 +0530

    new

--- 2119-A Number After a Double Reversal.py
+++ 2119-A Number After a Double Reversal.py
@@ -4,6 +4,8 @@ class Solution:
             return True      
         else:
             num=str(num)
+            #String strip() Method will remove the character from string
+            #[rstrip() will remove from right & lstrip() will remove from left]
             if len(num[::-1])==len(num.strip("0")):
                 return True
             else:return False
