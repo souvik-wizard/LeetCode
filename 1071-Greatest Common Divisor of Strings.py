a13546ac64c47667241d42bf3d266dcddf76f592
souvik-wizard <dassouvik3327@gmail.com>
2022-09-10 23:05:55 +0530

    addition

--- 1071-Greatest Common Divisor of Strings.py
+++ 1071-Greatest Common Divisor of Strings.py
@@ -2,3 +2,24 @@ from math import*
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
          return str1[:(str1+str2==str2+str1)*gcd(len(str1),len(str2))]
+
+
+
+# Easy to  understand sol
+from math import gcd
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+
+        if str1 == str2:
+            return str1
+
+        if str1+str2 != str2+str1:
+            return ""
+        
+        x = gcd(len(str1), len(str2))
+        
+        if str1[:x] == str2[:x]:
+            return str1[:x]
+        else:
+            return ""
--- 1780-Check if Number is a Sum of Powers of Three.py
+++ 1780-Check if Number is a Sum of Powers of Three.py
@@ -4,5 +4,4 @@ class Solution:
                 if n % 3 == 2:
                     return False
                 n = n // 3
-
             return True
