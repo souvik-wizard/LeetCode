8f05c3e057d158c12840d9a21dc7845deeb2102d
souvik-wizard <dassouvik3327@gmail.com>
2022-09-11 21:56:05 +0530

    Update 875-Koko Eating Bananas.py

--- 875-Koko Eating Bananas.py
+++ 875-Koko Eating Bananas.py
@@ -13,3 +13,13 @@ class Solution:
             else:
                 l = k + 1
         return res
+
+# Same type but less lines :)   
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low, high = 1, max(piles)
+        while low <= high:
+            k = (low + high) // 2
+            if sum(math.ceil(pile / k) for pile in piles) > h: low = k + 1
+            else: high = k - 1
+        return low
