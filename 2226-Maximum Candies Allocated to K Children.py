4509cff26a555aa5fce259aeb882bc0a33b25696
souvik-wizard <dassouvik3327@gmail.com>
2022-09-12 12:53:45 +0530

    Update 2226-Maximum Candies Allocated to K Children.py

--- 2226-Maximum Candies Allocated to K Children.py
+++ 2226-Maximum Candies Allocated to K Children.py
@@ -9,3 +9,8 @@ class Solution:
                 if sum(c//mid for c in candies)>=k: low=mid
                 else: high=mid-1
             return low
+
+# Using biscet (one line sol)
+class Solution:
+    def maximumCandies(self, C: List[int], k: int) -> int:
+        return bisect_left(range(1,sum(C)//k+1), True, key=lambda x:sum(c//x for c in C)<k)
