21e6e1a06caab0a687d49c0e088e2b28fedd4f14
souvik-wizard <dassouvik3327@gmail.com>
2022-09-19 19:28:39 +0530

    Create 2215-Find the Difference of Two Arrays.py

---
+++ 2215-Find the Difference of Two Arrays.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        nums1=list(set(nums1))
+        nums2=list(set(nums2))
+        for i in nums1[:]:
+            if i in nums2:
+                nums1.remove(i)
+                nums2.remove(i)
+        return [nums1,nums2]
+
+
+# 2 linear solutions
+# 1) 
+return [set(nums1)-set(nums2), set(nums2)-set(nums1)]
+# 2)
+return [[j for j in set(nums1) if j not in nums2],[i for i in set(nums2) if i not in nums1]]
+
