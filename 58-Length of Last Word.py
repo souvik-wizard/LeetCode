d4c04e9b8f62c03aa32927a20605196bf16cb1f6
souvik-wizard <dassouvik3327@gmail.com>
2022-10-15 13:23:54 +0530

    Update 58-Length of Last Word.py

--- 58-Length of Last Word.py
+++ 58-Length of Last Word.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
+        #strip will remove the space from start and end(if any)
         A=s.strip()
         B=A.split(" ")
         return len(B[-1])
